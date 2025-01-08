40d93d731d998931027a213384cc03d9efb538ce
mryurajverma73 <mryurajverma73@gmail.com>
2025-01-08 17:23:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mohitownerr')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/mohitownerr')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Itzofficialverma') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+OHjrcgklmlBjZTM1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+OHjrcgklmlBjZTM1')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/itzOfficialVerma') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/itzOfficialVerma')
 
 # True Or False
