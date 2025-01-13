e8d31967b2e6144edc9c89ad3df9334d7972ef51
mryurajverma73 <mryurajverma73@gmail.com>
2025-01-13 09:08:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -858,7 +858,7 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
     files, _, _ = await get_search_results(chat_id, search, max_results=10)
     files = [file for file in files if re.search(seas, file["file_name"], re.IGNORECASE)]
     
-    seas1 = "s01" if seas == "season 1" else "s02" if seas == "season 2" else "s03" if seas == "season 3" else "s04" if seas == "season 4" else "s05" if seas == "season 5" else "s06" if seas == "season 6" else "s07" if seas == "season 7" else "s08" if seas == "season 8" else "s09" if seas == "season 9" else "s10" if seas == "season 10" else ""
+    seas1 = "S01" if seas == "season 1" else "S02" if seas == "season 2" else "S03" if seas == "season 3" else "S04" if seas == "season 4" else "S05" if seas == "season 5" else "S06" if seas == "season 6" else "S07" if seas == "season 7" else "S08" if seas == "season 8" else "S09" if seas == "season 9" else "S10" if seas == "season 10" else ""
     search1 = f"{search1} {seas1}"
     BUTTONS1[key] = search1
     files1, _, _ = await get_search_results(chat_id, search1, max_results=10)
