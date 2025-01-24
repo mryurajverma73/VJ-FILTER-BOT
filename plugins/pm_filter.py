9a1a9ae497b375decf6b03d834af7bbf13073226
mryurajverma73 <mryurajverma73@gmail.com>
2025-01-24 13:53:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -824,7 +824,7 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
     except:
         pass
     seas = ''
-    season_search = ["s01","s02", "s03", "s04", "s05", "s06", "s07", "s08", "s09", "s10", "season 01","season 02","season 03","season 04","season 05","season 06","season 07","season 08","season 09","season 10", "season 1","season 2","season 3","season 4","season 5","season 6","season 7","season 8","season 9"]
+    season_search = ["S01","S02", "S03", "S04", "S05", "S06", "S07", "S08", "S09", "S10", "season 01","season 02","season 03","season 04","season 05","season 06","season 07","season 08","season 09","season 10", "season 1","season 2","season 3","season 4","season 5","season 6","season 7","season 8","season 9"]
     for x in range (len(season_search)):
         if season_search[x] in search:
             seas = season_search[x]
@@ -855,7 +855,7 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
     files, _, _ = await get_search_results(chat_id, search, max_results=10)
     files = [file for file in files if re.search(seas, file["file_name"], re.IGNORECASE)]
     
-    seas1 = "s01" if seas == "season 1" else "s02" if seas == "season 2" else "s03" if seas == "season 3" else "s04" if seas == "season 4" else "s05" if seas == "season 5" else "s06" if seas == "season 6" else "s07" if seas == "season 7" else "s08" if seas == "season 8" else "s09" if seas == "season 9" else "s10" if seas == "season 10" else ""
+    seas1 = "S01" if seas == "season 1" else "S02" if seas == "season 2" else "S03" if seas == "season 3" else "S04" if seas == "season 4" else "S05" if seas == "season 5" else "S06" if seas == "season 6" else "S07" if seas == "season 7" else "S08" if seas == "season 8" else "S09" if seas == "season 9" else "S10" if seas == "season 10" else ""
     search1 = f"{search1} {seas1}"
     BUTTONS1[key] = search1
     files1, _, _ = await get_search_results(chat_id, search1, max_results=10)
