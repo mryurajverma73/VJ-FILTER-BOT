6b7f2029aefa9aa4ded1ad9f53cd7cad17ce3980
mryurajverma73 <mryurajverma73@gmail.com>
2025-01-25 21:35:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,7 +30,8 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('👻 Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ 👻', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ],[
-            InlineKeyboardButton('😉 Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ😉', url=f'https://t.me/{SUPPORT_CHAT}'),
+            InlineKeyboardButton('😉 Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ😉', url=f'https://t.me/{SUPPORT_CHAT}')
+        ],[    
             InlineKeyboardButton('🍿 Mᴏᴠɪᴇ Gʀᴏᴜᴘ🍿', url=GRP_LNK)
         ],[
             InlineKeyboardButton('🥳 Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ 🥳', url=CHNL_LNK)
